ec84a22633c0e78099475293cc0be27a28021c35
yannisf <yannisf@work>
2017-11-17 15:35:07 +0200

    Scrapping based on htmlunit

--- src/test/java/bookproject/scrapper/ScrapperTest.java
+++ src/test/java/bookproject/scrapper/ScrapperTest.java
@@ -1,8 +1,10 @@
 package bookproject.scrapper;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.apache.commons.lang3.time.StopWatch;
 import org.junit.Test;
 import org.w3c.dom.Document;
-import org.w3c.tidy.Configuration;
 import org.w3c.tidy.Tidy;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -13,13 +15,12 @@ import javax.xml.xpath.XPathFactory;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Properties;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.concurrent.TimeUnit;
 
 public class ScrapperTest {
 
     private static final XPath X_PATH = XPathFactory.newInstance().newXPath();
-    private static final String BOOK_LINK_FROM_RESULT_EXPRESSION = "//a[@class=\"booklink\"][1]/@href";
+    private static final String BOOK_LINK_FROM_RESULT_EXPRESSION = "string(//a[@class=\"booklink\"][1]/@href)";
     private static final String TITLE_EXPRESSION = "string(//h1[@class=\"book_title\"])";
     private static final String AUTHOR_EXPRESSION = "string(//a[@class=\"booklink\" and starts-with(@href,\"/author/\")][1])";
     private static final String PUBLISHER_EXPRESSION = "string(//a[@class=\"booklink\" and starts-with(@href,\"/com/\")][1])";
@@ -27,12 +28,13 @@ public class ScrapperTest {
     private static final String SEARCH_FORMAT = BASE_URL + "/main.asp?page=results&isbn=%s";
 
     @Test
-    public void find() throws IOException, XPathExpressionException, ParserConfigurationException {
+    public void tidyFindTest() throws IOException, XPathExpressionException, ParserConfigurationException {
 //        String isbn = "9603291528";
 //        String isbn = "9789600316698";
 //        String isbn = "9789608965409";
         String isbn = "9789600403886";
 
+        StopWatch stopWatch = StopWatch.createStarted();
         String spec1 = String.format(SEARCH_FORMAT, isbn);
         URL searchUrl = new URL(spec1);
 
@@ -47,12 +49,13 @@ public class ScrapperTest {
         String title = getResult(document, TITLE_EXPRESSION);
         String author = getResult(document, AUTHOR_EXPRESSION);
         String publisher = getResult(document, PUBLISHER_EXPRESSION);
+        System.out.println("XPATH finished in " + stopWatch.getTime(TimeUnit.MILLISECONDS));
 
         System.out.println(String.format("spec1: %s", spec1));
         System.out.println(String.format("spec2: %s", spec2));
         System.out.println(String.format("%s: %s, %s, %s", isbn, title, author, publisher));
 
-    //        String expectedTitle = "Το λάθος";
+        //        String expectedTitle = "Το λάθος";
 //        String expectedAuthor = "Αντώνης Σαμαράκης";
 //        String expectedPublisher = "Εκδόσεις Καστανιώτη";
 //        assertThat(title).isEqualTo(expectedTitle);
@@ -74,6 +77,31 @@ public class ScrapperTest {
 
     private String getResult(Document document, String expression) throws XPathExpressionException {
         return (String) X_PATH.compile(expression).evaluate(document, XPathConstants.STRING);
+    }
+
+    @Test
+    public void htmlUnitFindTest() throws IOException {
+        String isbn = "9789600403886";
+
+        StopWatch stopWatch = StopWatch.createStarted();
+        WebClient client = new WebClient();
+        client.getOptions().setCssEnabled(false);
+        client.getOptions().setJavaScriptEnabled(false);
+
+        String spec1 = String.format(SEARCH_FORMAT, isbn);
+        HtmlPage page1 = client.getPage(spec1);
+        String link = page1.getFirstByXPath(BOOK_LINK_FROM_RESULT_EXPRESSION);
+
+        String spec2 = BASE_URL + link;
+        HtmlPage page2 = client.getPage(spec2);
+        String title = page2.getFirstByXPath(TITLE_EXPRESSION);
+        String author = page2.getFirstByXPath(AUTHOR_EXPRESSION);
+        String publisher = page2.getFirstByXPath(PUBLISHER_EXPRESSION);
+        System.out.println("XPATH finished in " + stopWatch.getTime(TimeUnit.MILLISECONDS));
+
+        System.out.println(String.format("spec1: %s", spec1));
+        System.out.println(String.format("spec2: %s", spec2));
+        System.out.println(String.format("%s: %s, %s, %s", isbn, title, author, publisher));
     }
 
 }

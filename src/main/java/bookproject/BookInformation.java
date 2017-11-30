9368f553720c7481e35a4d0d431ba954da312704
yannis <yannis@ianos>
2017-11-30 23:44:21 +0200

    Server H2

--- src/main/java/bookproject/BookInformation.java
+++ src/main/java/bookproject/BookInformation.java
@@ -4,18 +4,29 @@ import lombok.Builder;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
 
 @Entity
 @Data
 @Builder
-public class BookInformation {
+public class BookInformation implements Serializable {
 
+    @Id
+    @GeneratedValue
     private Long id;
+
     private String isbn;
+
     private String isbn13;
+
     private String title;
+
     private String author;
+
     private String publisher;
+
     private String url;
 
 }
